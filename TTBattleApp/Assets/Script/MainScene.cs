ec2126dcc37a35cd0ecb46cc110ef2a734c0b403
koooota <eno6k21@gmail.com>
2019-10-19 17:29:20 +0900

    trainsition title to main

---
+++ TTBattleApp/Assets/Script/MainScene.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System;
+
+public class MainScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // 対戦開始。バトルシーンに移動
+    public void BattleStart()
+    {
+        SceneManager.LoadScene("BattleScene");
+    }
+
+}
---
+++ TTBattleApp/Assets/Script/TitleScrene.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TitleScrene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ToMenuScene()
+    {
+        SceneManager.LoadScene("MainScene");
+
+    }
+
+}
