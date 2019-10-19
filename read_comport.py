17245be6117899276485972265320a862da41917
kitsystemyou <kitsystemyou@gmail.com>
2019-10-20 01:40:08 +0900

    catch signal of sensor

--- read_comport.py
+++ read_comport.py
@@ -6,6 +6,8 @@ import struct
 import time
 import threading
 import serial
+import requests
+import csv
 
 # Global settings of for the serial port
 comPort = "COM3"
@@ -25,19 +27,27 @@ wavFile.setparams((nchannels, sampwidth, sampleRate, nframes, comptype, compname
 serInstance = serial.Serial(comPort, baudrate, timeout=0)
 # Buffer the last value in case of data dropping​
 lastValueFromStream = b''
-print("end")
+print("pretend")
 
 def processAudioData():
     # Create a byte object for storing the returned values from the serial port
     returnedValue = b''
     # Variable to store the available bytes from the serial buffer
     bytesToRead = 1
+    count = 0
+    csv_data = []
     while bytesToRead != 0:
         global lastValueFromStream
         # Check whether bytes are available for reading
         bytesToRead = serInstance.inWaiting()
+
         # If we have nothing to do, just leave the while loop
+        if count == 11000:
+            wavFile.close()
+            print("success")
+            break
         if bytesToRead == 0:
+            wavFile.close()
             break
         # Read the bytes and save them in the returnedValue variable
         returnedValue = serInstance.read(bytesToRead)
@@ -45,6 +55,12 @@ def processAudioData():
         returnedValue = returnedValue.split(b'\n')
         # Process the splitted values
         for value in returnedValue:
+            if count == 11000:
+                # print("pre")
+                # wavFile.close()
+                # print("after")
+                break
+    
             # We send three bytes - if we receive less or more, we drop the result
             if len(value) != 3:
                 #continue
@@ -55,15 +71,32 @@ def processAudioData():
             # Due to transmission errors, there might be values above/below the maximum value
             # We receive 20 bit signed integer values which have a maximum value of (2^19-1) and minimum value (-2^19)
             if integer < -524288 or integer > 524287:
+                print("crazy")
                 continue
+
             # Parse the integer to a signed 32 bit value with maximum value of +/-2^31-1 = 2147483647
             # Note that there is an error of +/- 1 due to asymmetric max. values
             parse = int(float((float(integer)/524288)*(2147483647)))
             # Write the values to the wave file
             wavFile.writeframes(struct.pack('i', parse))
+            # print(parse)
             print("test")
+            count += 1
+            csv_data.append(parse)
+
+            # send request to webhook
+            # response = requests.post('https://maker.ifttt.com/trigger/Player1_TT/with/key/hnXromm5rMulX5fW03d7s', data={'value1': parse})
+            # print(response.status_code)    # HTTPのステータスコード取得
+            # print(response.text)
+        with open('view_data.csv', 'w') as f:
+            writer = csv.writer(f)
+            writer.writerow(csv_data)
     return 
 
 if __name__ == '__main__':
     while True:
         processAudioData()
+        break
+    serInstance.close()
+
+
