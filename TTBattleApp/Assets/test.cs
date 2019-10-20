af1300da8372877f9b3d5e205ae0b99798428d8a
Yamada Tatsuhiko <o236075t@mail.kyutech.jp>
2019-10-20 10:52:59 +0900

    10201052

--- TTBattleApp/Assets/test.cs
+++ TTBattleApp/Assets/test.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FirebaseRealtimeDatabaseSample1 : MonoBehaviour
+public class test : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
