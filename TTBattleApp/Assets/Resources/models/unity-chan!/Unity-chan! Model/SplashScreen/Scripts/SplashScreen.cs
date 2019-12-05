50510dc552b31ad96fe1ae1af56cd58b59c9f6c1
koooota <eno6k21@gmail.com>
2019-12-05 20:21:54 +0900

    udp connect & file arrangement

---
+++ TTBattleApp/Assets/Resources/models/unity-chan!/Unity-chan! Model/SplashScreen/Scripts/SplashScreen.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace UnityChan
+{
+	[ExecuteInEditMode]
+	public class SplashScreen : MonoBehaviour
+	{
+        void NextLevel() => Application.LoadLevel(Application.loadedLevel + 1);
+    }
+}
